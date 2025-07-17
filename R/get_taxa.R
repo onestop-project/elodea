68ec3e22c01a73c2413bf647d03ea10e783d749f
Sanne Govaert <44606923+sannegovaert@users.noreply.github.com>
2025-07-17 15:53:27 +0200

    Update NAMESPACE

---
+++ R/get_taxa.R
@@ -0,0 +1,33 @@
+#' Get taxa from checklist
+#'
+#' @param datasetKey GBIF dataset key.
+#'
+#' @returns Taxa from checklist.
+#' @export
+#' @examples
+#' # Belgium
+#' datasetKey <- "6d9e952f-948c-4483-9807-575348147c7e"
+#' get_taxa(datasetKey)
+#' # Cyprus
+#' datasetKey <- "2f7ea7d1-a73f-46f6-b790-7339126a999f"
+#' get_taxa(datasetKey)
+#' # Andorra
+#' datasetKey <- "016c16c3-d907-4c88-97dd-97ad62c8130e"
+#' get_taxa(datasetKey)
+get_taxa <- function(datasetKey) {
+  taxa_raw <- rgbif::name_usage(datasetKey = datasetKey, limit = 99999)$data
+  taxa <-
+    taxa_raw %>%
+    # Keep only source taxa, not denormed higher classification taxa
+    dplyr::filter(origin == "SOURCE") %>%
+    dplyr::select(
+      key, taxonID, scientificName, acceptedKey, accepted, kingdom, rank
+    ) %>%
+    #TODO: column acceptedKey not always present
+    dplyr::rename(
+      taxonKey = key,
+      taxonRank = rank
+      ) %>%
+    dplyr::as_tibble()
+  return(taxa)
+}
