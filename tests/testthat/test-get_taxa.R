0fd6ad69aa707251bde2b2ccb80febee24dd8d74
Sanne Govaert <44606923+sannegovaert@users.noreply.github.com>
2025-07-17 15:53:32 +0200

    Create test-get_taxa.R

---
+++ tests/testthat/test-get_taxa.R
@@ -0,0 +1,27 @@
+test_that(
+  "get_taxa() returns correct taxa data frame", {
+    skip_if_offline()
+    datasetKey <- "016c16c3-d907-4c88-97dd-97ad62c8130e"
+    taxa <- get_taxa(datasetKey)
+
+    # The returned output is of type list
+    expect_type(taxa, "list")
+    # The returned output is a tibble data.frame
+    expect_equal(
+      class(taxa),
+      c("tbl_df", "tbl", "data.frame")
+    )
+    # Check that the output has the expected columns
+    expect_equal(
+      c(
+        "taxonKey",
+        "taxonID",
+        "scientificName",
+        "acceptedKey",
+        "accepted",
+        "kingdom",
+        "taxonRank"
+        ),
+      names(taxa)
+    )
+  })
