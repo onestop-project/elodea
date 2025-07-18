c24faf4b81546db4f1c24b7877fb6cb97dcc49ea
Sanne Govaert <44606923+sannegovaert@users.noreply.github.com>
2025-07-18 14:38:02 +0200

    Create get_distributions.R

---
+++ R/get_distributions.R
@@ -0,0 +1,100 @@
+#' Gets distributions
+#'
+#' @inheritParams get_taxa
+#'
+#' @returns A data frame with the following columns:
+#' - taxonKey
+#' - taxonID
+#' - countryCode
+#' - occurrenceStatus
+#' - establishmentMeans
+#' - degreeOfEstablishment
+#' - pathway
+#' - eventDate
+#' - source
+#' @export
+#'
+#' @examples
+#' # Belgium
+#' datasetKey <- "6d9e952f-948c-4483-9807-575348147c7e"
+#' get_distributions(datasetKey)
+#' # Cyprus
+#' datasetKey <- "2f7ea7d1-a73f-46f6-b790-7339126a999f"
+#' get_distributions(datasetKey)
+#' # Andorra
+#' datasetKey <- "016c16c3-d907-4c88-97dd-97ad62c8130e"
+#' get_distributions(datasetKey)
+get_distributions <- function(datasetKey) {
+  taxa <- get_taxa(datasetKey)
+  taxon_keys <- dplyr::pull(taxa, taxonKey)
+
+  # Download distributions with progress bar
+  progressr::with_progress({
+    progress_bar <- progressr::progressor(steps = length(taxon_keys))
+    distributions <-
+      furrr::future_map_dfr(
+        taxon_keys,
+        function(x) {
+          progress_bar()
+          rgbif::name_usage(
+            key = x,
+            data = "distribution"
+          )$data
+        },
+        .options = furrr::furrr_options(seed = TRUE)
+      )
+  })
+
+  # Clean distributions
+  if ("status" %in% names(distributions)) {
+    distributions <-
+      distributions %>%
+      dplyr::rename(occurrenceStatus = status)
+  }
+  if ("country" %in% names(distributions)) {
+    distributions <-
+      distributions %>%
+      dplyr::rename(countryCode = country)
+  }
+  distributions <-
+    distributions %>%
+    mutate_when_missing(occurrenceStatus = "present") %>%
+    mutate_when_missing(establishmentMeans = NA_character_) %>%
+    mutate_when_missing(degreeOfEstablishment = NA_character_) %>%
+    mutate_when_missing(pathway = NA_character_) %>%
+    mutate_when_missing(eventDate = NA_character_) %>%
+    mutate_when_missing(source = NA_character_) %>%
+    dplyr::select(
+      taxonKey,
+      countryCode,
+      occurrenceStatus,
+      establishmentMeans,
+      degreeOfEstablishment,
+      pathway,
+      eventDate,
+      source
+    )
+
+  # Join taxa and distributions
+  result <- taxa %>%
+    dplyr::full_join(
+      distributions,
+      by = c("taxonKey"),
+      keep = FALSE,
+      multiple = "all",
+      relationship = "many-to-many"
+      ) %>%
+    dplyr::select(
+      taxonKey,
+      taxonID,
+      countryCode,
+      occurrenceStatus,
+      establishmentMeans,
+      degreeOfEstablishment,
+      pathway,
+      eventDate,
+      source
+      )
+
+  return(result)
+}
