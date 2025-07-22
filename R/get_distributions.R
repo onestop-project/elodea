d0348da564e515ffb99a0953e47a3087d3330454
Sanne Govaert <44606923+sannegovaert@users.noreply.github.com>
2025-07-22 11:19:20 +0200

    update examples

--- R/get_distributions.R
+++ R/get_distributions.R
@@ -16,14 +16,11 @@
 #'
 #' @examples
 #' # Belgium
-#' datasetKey <- "6d9e952f-948c-4483-9807-575348147c7e"
-#' get_distributions(datasetKey)
+#' distributions_BE <- get_distributions("6d9e952f-948c-4483-9807-575348147c7e")
 #' # Cyprus
-#' datasetKey <- "2f7ea7d1-a73f-46f6-b790-7339126a999f"
-#' get_distributions(datasetKey)
+#' distributions_CY <- get_distributions("2f7ea7d1-a73f-46f6-b790-7339126a999f")
 #' # Andorra
-#' datasetKey <- "016c16c3-d907-4c88-97dd-97ad62c8130e"
-#' get_distributions(datasetKey)
+#' distributions_AD <- get_distributions("016c16c3-d907-4c88-97dd-97ad62c8130e")
 get_distributions <- function(datasetKey) {
   taxa <- get_taxa(datasetKey)
   taxon_keys <- dplyr::pull(taxa, "taxonKey")
--- R/get_taxa.R
+++ R/get_taxa.R
@@ -6,14 +6,11 @@
 #' @export
 #' @examples
 #' # Belgium
-#' datasetKey <- "6d9e952f-948c-4483-9807-575348147c7e"
-#' get_taxa(datasetKey)
+#' taxa_BE <- get_taxa("6d9e952f-948c-4483-9807-575348147c7e")
 #' # Cyprus
-#' datasetKey <- "2f7ea7d1-a73f-46f6-b790-7339126a999f"
-#' get_taxa(datasetKey)
+#' taxa_CY <- get_taxa("2f7ea7d1-a73f-46f6-b790-7339126a999f")
 #' # Andorra
-#' datasetKey <- "016c16c3-d907-4c88-97dd-97ad62c8130e"
-#' get_taxa(datasetKey)
+#' taxa_AD <- get_taxa("016c16c3-d907-4c88-97dd-97ad62c8130e")
 get_taxa <- function(datasetKey) {
   taxa_raw <- rgbif::name_usage(datasetKey = datasetKey, limit = 99999)$data
   taxa <-
