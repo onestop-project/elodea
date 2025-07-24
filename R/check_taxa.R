a522ea30970798d048ba7941d0f7354e00a039f0
Sanne Govaert <44606923+sannegovaert@users.noreply.github.com>
2025-07-24 16:34:20 +0200

    Create check_taxa.R

---
+++ R/check_taxa.R
@@ -0,0 +1,34 @@
+#' Checks taxa
+#'
+#' @param taxa
+#'
+#' @returns `taxa` invisibly or an error.
+#' @export
+check_taxa <- function(taxa) {
+  # Check if taxa is a data frame
+  if (!is.data.frame(taxa)) {
+    cli::cli_abort(
+      "{.arg taxa} must be a data frame.",
+      class = "elodea_error_taxa_invalid_class"
+    )
+  }
+
+  # Check required columns
+  required_columns <- c(
+    "taxonKey", "nubKey", "taxonID", "scientificName", "acceptedKey",
+    "accepted", "kingdom", "taxonRank"
+    )
+  missing_columns <- setdiff(required_columns, names(taxa))
+
+  if (length(missing_columns) > 0) {
+      cli::cli_abort(
+        c(
+          "{.arg taxa} is missing 1 or more required columns.",
+          "x" = "The following columns are missing: {.val {missing_columns}}."
+        ),
+        class = "elodea_error_taxa_missing_columns"
+      )
+  }
+
+  invisible(taxa)
+}
