b136788da4281d70fd3bdd73ace01531ed31ac73
Sanne Govaert <44606923+sannegovaert@users.noreply.github.com>
2025-09-23 16:07:56 +0200

    Add test

--- tests/testthat/test-filter_data.R
+++ tests/testthat/test-filter_data.R
@@ -51,3 +51,202 @@ test_that("filter_data() returns the expected files for an existing dataset", {
   )
   expect_equal(names(output$notes), notes_expected_columns)
 })
+
+test_that("filter_data() returns the expected output for a dummy dataset", {
+  taxa <-
+    # 1. All good
+    data.frame(
+      taxonKey = 148674360,
+      nubKey = 1047536,
+      taxonID = "all_good",
+      scientificName = "Leptinotarsa decemlineata (Say, 1824)",
+      acceptedKey = NA_real_,
+      accepted = NA_character_,
+      kingdom = "Animalia",
+      taxonRank = "species"
+    ) %>%
+    # 2. `scientificName` need to be replaced with correct backbone name
+    tibble::add_row(
+      taxonKey = 148674355,
+      nubKey = 8411684,
+      taxonID = "scientificname_different_from_backbone",
+      scientificName = "Monochamus sutor (LinnÃ©, 1758)",
+      acceptedKey = NA_real_,
+      accepted = NA_character_,
+      kingdom = "Animalia",
+      taxonRank = "species"
+    ) %>%
+    # 3. `key`s and `scientificName` need to be replaced with accepted values
+    tibble::add_row(
+      taxonKey = 148674371,
+      nubKey = 3204008,
+      taxonID = "synonym",
+      scientificName = "Pseudoperonospora humuli (Miyabe & Takah.) G.W.Wilson",
+      acceptedKey = 201367230,
+      accepted = "Pseudoperonospora cubensis (Berk. & M.A.Curtis) Rostovzev",
+      kingdom = "Chromista",
+      taxonRank = "species"
+    ) %>%
+    # 4. Record needs to be removed because `nubKey` is missing
+    tibble::add_row(
+      taxonKey = 148746536,
+      nubKey = NA,
+      taxonID = "nubkey_missing",
+      scientificName = "Pinus strobus",
+      acceptedKey = NA_real_,
+      accepted = NA_character_,
+      kingdom = "Plantae",
+      taxonRank = "species"
+    )
+
+  distributions <-
+    # 1. All good
+    data.frame(
+      taxonKey = 148674360,
+      countryCode = "AD",
+      occurrenceStatus = "present",
+      establishmentMeans = "introduced",
+      degreeOfEstablishment = NA_character_,
+      pathway = NA_character_,
+      eventDate = NA_character_,
+      source = NA_character_
+    ) %>%
+    # 2. All good
+    tibble::add_row(
+      taxonKey = 148674355,
+      countryCode = "AD",
+      occurrenceStatus = "present",
+      establishmentMeans = "introduced",
+      degreeOfEstablishment = NA_character_,
+      pathway = NA_character_,
+      eventDate = NA_character_,
+      source = NA_character_
+    ) %>%
+    # 3. All good
+    tibble::add_row(
+      taxonKey = 148674371,
+      countryCode = "AD",
+      occurrenceStatus = "present",
+      establishmentMeans = "introduced",
+      degreeOfEstablishment = NA_character_,
+      pathway = NA_character_,
+      eventDate = NA_character_,
+      source = NA_character_
+    ) %>%
+    # 4. Record needs to be removed because `nubKey` is missing
+    tibble::add_row(
+      taxonKey = 148746536,
+      countryCode = "AT",
+      occurrenceStatus = "present",
+      establishmentMeans = "introduced",
+      degreeOfEstablishment = NA_character_,
+      pathway = NA_character_,
+      eventDate = NA_character_,
+      source = NA_character_
+    )
+
+  expected_taxa <-
+    # 1. All good
+    data.frame(
+      taxonKey = 148674360,
+      nubKey = 1047536,
+      taxonID = "all_good",
+      scientificName = "Leptinotarsa decemlineata (Say, 1824)",
+      acceptedKey = NA_real_,
+      accepted = NA_character_,
+      kingdom = "Animalia",
+      taxonRank = "species"
+    ) %>%
+    # 2. `scientificName` is replaced with correct backbone name
+    tibble::add_row(
+      taxonKey = 148674355,
+      nubKey = 8411684,
+      taxonID = "scientificname_different_from_backbone",
+      scientificName = "Monochamus sutor (Linnaeus, 1758)",
+      acceptedKey = NA_real_,
+      accepted = NA_character_,
+      kingdom = "Animalia",
+      taxonRank = "species"
+    ) %>%
+    # 3. `key`s and `scientificName` are replaced with accepted values
+    tibble::add_row(
+      taxonKey = 201367230,
+      nubKey = 3204007,
+      taxonID = NA,
+      scientificName =
+        "Pseudoperonospora cubensis (Berk. & M.A.Curtis) Rostovzev",
+      acceptedKey = NA_real_,
+      accepted = NA_character_,
+      kingdom = "Chromista",
+      taxonRank = "species"
+    ) %>%
+    tibble::as_tibble()
+
+  expected_distributions <-
+    # 1. All good
+    data.frame(
+      taxonKey = 148674360,
+      countryCode = "AD",
+      occurrenceStatus = "present",
+      establishmentMeans = "introduced",
+      degreeOfEstablishment = NA_character_,
+      pathway = NA_character_,
+      eventDate = NA_character_,
+      source = NA_character_
+    ) %>%
+    # 2. All good
+    tibble::add_row(
+      taxonKey = 148674355,
+      countryCode = "AD",
+      occurrenceStatus = "present",
+      establishmentMeans = "introduced",
+      degreeOfEstablishment = NA_character_,
+      pathway = NA_character_,
+      eventDate = NA_character_,
+      source = NA_character_
+    ) %>%
+    # 3. All good
+    tibble::add_row(
+      taxonKey = 148674371,
+      countryCode = "AD",
+      occurrenceStatus = "present",
+      establishmentMeans = "introduced",
+      degreeOfEstablishment = NA_character_,
+      pathway = NA_character_,
+      eventDate = NA_character_,
+      source = NA_character_
+    ) %>%
+    tibble::as_tibble()
+
+  expected_notes <-
+    data.frame(
+      taxonID = "scientificname_different_from_backbone",
+      taxonKey = 148674355,
+      scientificName = "Monochamus sutor (LinnÃ©, 1758)",
+      action = "scientificName_replaced_by_backbone_name",
+      acceptedKey = NA_real_,
+      accepted = NA_character_
+    ) %>%
+    tibble::add_row(
+      taxonID = "synonym",
+      taxonKey = 148674371,
+      scientificName = "Pseudoperonospora humuli (Miyabe & Takah.) G.W.Wilson",
+      action = "merged_with_accepted",
+      acceptedKey = 201367230,
+      accepted = "Pseudoperonospora cubensis (Berk. & M.A.Curtis) Rostovzev"
+    ) %>%
+    tibble::add_row(
+      taxonID = "nubkey_missing",
+      taxonKey = 148746536,
+      scientificName = "Pinus strobus",
+      action = "not_matched_with_backbone",
+      acceptedKey = NA_real_,
+      accepted = NA_character_
+    ) %>%
+    tibble::as_tibble()
+
+  output <- filter_data(taxa, distributions)
+  expect_equal(output$taxa, expected_taxa)
+  # expect_equal(output$distributions, expected_distributions) # Failing: taxonKey need to be replaced with accepted key
+  expect_equal(output$notes, expected_notes)
+})
