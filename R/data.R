3d602702c20398231eadf1ab3e59bb248768cc04
Sanne Govaert <44606923+sannegovaert@users.noreply.github.com>
2026-03-31 14:43:52 +0200

    Update documentation

--- R/data.R
+++ R/data.R
@@ -3,6 +3,10 @@
 #' A small example checklist of alien species. The source checklist is maintained
 #' in an online Google sheet, and published to the web as a CSV file.
 #'
+#' `example_checklist` is a CSV file with 12 records, including some taxa with
+#' misspelled names and some with names that are not in the GBIF backbone
+#' taxonomy, to showcase the functionality of `elodea`.
+#'
 #' @source Online [Google sheet](https://docs.google.com/spreadsheets/d/1Z2fsQJ3mUPLowlqAd5W5gzBeDVv80HTwhqscKZ7tYPg/edit?usp=sharing).
 #' @family sample data
 #' @examples
