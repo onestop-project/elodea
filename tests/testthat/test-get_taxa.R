86bb7f83b32e3db7186ce630caa5fdd244c91e2e
Sanne Govaert <44606923+sannegovaert@users.noreply.github.com>
2026-04-17 11:48:40 +0200

    Remove slow test

--- tests/testthat/test-get_taxa.R
+++ tests/testthat/test-get_taxa.R
@@ -9,14 +9,11 @@ test_that("get_taxa() warns on invalid datasetKey", {
 test_that("get_taxa() returns correct taxa data frame", {
   skip_if_offline()
   taxa_andorra <- get_taxa("016c16c3-d907-4c88-97dd-97ad62c8130e")
-  taxa_belgium <- get_taxa("6d9e952f-948c-4483-9807-575348147c7e")
 
   # The returned output is of type list
   expect_type(taxa_andorra, "list")
-  expect_type(taxa_belgium, "list")
   # The returned output is a tibble data.frame
   expect_equal(class(taxa_andorra), c("tbl_df", "tbl", "data.frame"))
-  expect_equal(class(taxa_belgium), c("tbl_df", "tbl", "data.frame"))
   # Check that the output has the expected columns
   expected_columns <- c(
     "taxonKey",
@@ -30,7 +27,6 @@ test_that("get_taxa() returns correct taxa data frame", {
 
   )
   expect_equal(expected_columns, names(taxa_andorra))
-  expect_equal(expected_columns, names(taxa_belgium))
 })
 
 test_that("get_taxa() filters on origin", {
