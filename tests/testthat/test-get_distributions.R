84c7a2981883198ca0bb3216044003b2997cf25d
Damiano Oldoni <damiano.oldoni@inbo.be>
2026-04-23 17:00:09 +0200

    Remove duplicated line

--- tests/testthat/test-get_distributions.R
+++ tests/testthat/test-get_distributions.R
@@ -1,6 +1,5 @@
 test_that("get_distributions() returns correct distributions data frame", {
   skip_if_offline()
-  skip_if_offline()
   vcr::local_cassette("distributions_andorra")
 
   distr_andorra <- get_distributions("016c16c3-d907-4c88-97dd-97ad62c8130e")
